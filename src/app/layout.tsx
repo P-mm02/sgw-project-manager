d645f610fd668d34d595286e07164a64f643f064
P-mm02 <poomtawee.mm02@gmail.com>
2025-07-02 08:56:41 +0700

    mobile size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
+// src/app/layout.tsx
 import '@/styles/globals.css'
 import Nav from './components/Nav/Nav'
 import Footer from './components/Footer/Footer'
 
-export default async function RootLayout({
+export const metadata = {
+  title: 'SGW-Working',
+  description: 'SGW ERP website build by Wee',
+  viewport: 'width=device-width, initial-scale=1', // ✅ Add this line
+}
+
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-
   return (
     <html lang="en">
       <body>
