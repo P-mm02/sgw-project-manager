996f8e893cc49303e507a6888ba11c61f0608e3b
P-mm02 <poomtawee.mm02@gmail.com>
2025-07-03 10:30:22 +0700

    Debuging Line Noti

--- src/lib/sendMessage/handleLicenseNotifications.ts
+++ src/lib/sendMessage/handleLicenseNotifications.ts
@@ -5,20 +5,22 @@ import { sendLineMessage } from '@/lib/sendMessage/sendLineMessage'
 
 export async function handleLicenseNotifications(licenses: LicenseType[]) {
   const today = new Date()
-  const userIds = [
-    'C0fc7a3d81a3b48dac518739672356f1d',
-  ]
-/*   const userIds = [
+//SGW weeID
+  const userIds = ['C0fc7a3d81a3b48dac518739672356f1d']
+  //SGW GroupID SG แจ้งเตือน
+  /*   const userIds = [
     'C0fc7a3d81a3b48dac518739672356f1d',  
   ]
  */
-/*   const userIds = [
+  //SGW userIds
+  /*   const userIds = [
     'C0fc7a3d81a3b48dac518739672356f1d',
     'U7954973400f148caf34e524adc4db5ac',
     'U45649660c3ff9de01615478639509a76',
     'U8bb70a35218c0ac670343d5449a60ab6',
   ] */
-/*   const userIds = [
+  //SGW-TEST userIds
+  /*   const userIds = [
     'Ua053de08814ccd75375a472e6a404f3e',
     'U4a74a0e50ecbc9807f53b020a56a7951',
   ] */
@@ -43,13 +45,9 @@ export async function handleLicenseNotifications(licenses: LicenseType[]) {
     const message = `ทดสอบข้อความอัตโนมัติ แจ้งเตือนใบอนุญาติใกล้หมดอายุ
     ⚠️ ${license.clientName}\n
     ใบอนุญาตเลขที่\n
-    ${
-      license.licenseNumber
-    }\n
+    ${license.licenseNumber}\n
     จะหมดอายุ วันที่\n
-    ${formatDateToThai(
-      expireDate
-    )}\n
+    ${formatDateToThai(expireDate)}\n
     📌 เตรียมดำเนินการต่ออายุใบอนุญาต`
 
     try {
